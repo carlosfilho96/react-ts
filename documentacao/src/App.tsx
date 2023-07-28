ce32218bfa79a8abc9a96df37d55ba2249411e65
Carlos <carlosalmeidafilho96@gmail.com>
2023-07-27 22:53:08 -0300

    270720232251
    Subindo alterações da aula, parei em 21:02 / 37:38
    
    https://www.youtube.com/watch?v=tG3Uwvuqzuk&ab_channel=Jo%C3%A3oBibiano

--- documentacao/src/App.tsx
+++ documentacao/src/App.tsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+// Aula: https://www.youtube.com/watch?v=tG3Uwvuqzuk&ab_channel=Jo%C3%A3oBibiano
+
+// const Example = (props) => {
+//   return <h1>{props.text}</h1>;
+// }
+
+// Tipagem: 3 formas para definir tipos
+
+// Forma 1:
+
+type TitleProps = {
+  text: string;
+};
+
+const Title = ({ text }: TitleProps) => { // O objeto text é do tipo TitleProps
+  return <h1>{text}</h1>;                 
+};
+
+// Forma 2:
+interface TitleProps2 {
+  text: string;  
+}
+
+const Title2 = ({ text }: TitleProps2) => { // O objeto text é do tipo TitleProps2
+  return <h1>{text}</h1>;                 
+};
+
+// Forma 3:
+const Title3 = ({ text }: { text: string }) => { // Tem-se um objeto text que é a desestruturação
+  return <h1>{text}</h1>;                        // e outro objeto que é o tipo
+};
+
+// Como passar o texto como um filho, e não como uma propriedade
+const Title4 = ({ children }: { children: React.ReactNode }) => { 
+  return <h1>{children}</h1>;                        
+};
+
+// Como definir propriedades default
+type TitleProps3 = {
+  children: React.ReactNode;
+  size: "small" | "large";
+};
+
+const Title5 = ({ children, size = "large" }: TitleProps3) => { // O objeto text é do tipo TitleProps3
+  return (
+    <h1 style={{fontSize: size == "small" ? "1.5rem" : "3rem"}}>
+      {children}
+    </h1>
+  );                 
+};
+
+// Como compor props (1 Forma)
+const Title6 = ({ children, size = "large" }: { children: React.ReactNode; size?: "small" | "large"}) => { // O objeto text é do tipo TitleProps3
+  return (
+    <h1 style={{fontSize: size == "small" ? "1.5rem" : "3rem"}}>
+      {children}
+    </h1>
+  );                 
+};
+
+// Como compor props (2 Forma)
+const Title7 = ({ children, size = "large" }: { children: React.ReactNode; size?: "small" | "large"}) => { // O objeto text é do tipo TitleProps3
+  return (
+    <h1 style={{fontSize: size == "small" ? "1.5rem" : "3rem"}}>
+      {children}
+    </h1>
+  );                 
+};
+
+const titleDefaultProps = {
+  size: "small"
+};
+
+Title7.defaultProps = titleDefaultProps;
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <Title text="Hello World" />
+      <Title2 text="Hello World" />
+      <Title3 text="Hello World" />
+      <Title4>
+        <span>
+          Hello <b>World</b>
+        </span>
+      </Title4>
+      <Title5 size="small">
+        <span>
+          Hello <b>World</b>
+        </span>
+      </Title5>
+      <Title6 size="small">
+        <span>
+          Hello <b>World</b>
+        </span>
+      </Title6>     
+      <Title7>
+        <span>
+          Hello <b>World</b>
+        </span>
+      </Title7>        
+    </div>
+
+  );
 }
 
 export default App
